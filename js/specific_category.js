f8f379747989fd9a61e0f36fe7aec4b5b0878597
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-02-17 23:38:37 +0100

    finishes dynamics

--- js/specific_category.js
+++ js/specific_category.js
@@ -1,82 +1,75 @@
 // //  ${recipes.ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}  mapper array inde i array
 
-let listContainer = document.querySelector(".listcontainer");
+let listContainer = document.querySelector(".favorites");
+let listContainer1 = document.querySelector(".oneRecipe");
+let listContainer2 = document.querySelector(".fullArray");
+
 const mycategory = new URLSearchParams(window.location.search).get("tag");
 
-fetch(`https://dummyjson.com/recipes?tag=Italian`)
+console.log(mycategory);
+
+fetch(`https://dummyjson.com/recipes/tag/${mycategory}`)
   .then((response) => response.json())
   .then((data) => {
     showFavorites(data.recipes);
+    showOneRecipe(data.recipes);
+    showFullArray(data.recipes);
   });
 
 function showFavorites(data) {
-  // const filteredRecipes = data.filter((recipes) => recipes.tags.includes(mycategory));
-
   const markup = data
     .map(
       (recipes) =>
         `
-        <div class="favorites">
-          <h2>Favoritter</h2>
-          <img src="${recipes.image}" alt="" />
+          <div>          
           <img src="${recipes.image}" alt="" />
           <h3>${recipes.name}</h3>
-          <h3>${recipes.name}</h3>
-        </div>
+          </div>
+          `
+    )
+    .join("");
+  listContainer.innerHTML = markup;
+}
+
+function showOneRecipe(data) {
+  const markup = data
+    .map(
+      (recipes) =>
+        `
+    <section class="second">
+    
+<img src="${recipes.image}" alt="" />
+<h3>${recipes.name}</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti doloremque ex dolorum incidunt neque voluptatibus ipsam laboriosam earum.</p>
+    </section>
+
       `
     )
     .join("");
-  console.log(markup);
-
-  listContainer.innerHTML = markup;
+  listContainer1.innerHTML = markup;
 }
-document.querySelector(".listcontainer").innerHTML = mycategory;
 
-// let listContainer = document.querySelector(".listcontainer");
-// const mycategory = new URLSearchParams(window.location.search).get("tag");
+function showFullArray(data) {
+  let markup = `<h2>${mycategory}</h2>`;
 
-// fetch(`https://dummyjson.com/recipes/tags?tag=${mycategory}`) // Hent opskrifter baseret på tag
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log("API Response:", data.recipes); // Debugging
-//     showFavorites(data.recipes); // Kald funktionen med opskrifter
-//   });
+  // Wrapper til opskrifter
+  markup += `<div class="favorites">`;
 
-// function showFavorites(data) {
-//   // Filtrér opskrifterne baseret på `mycategory`
-//   const filteredRecipes = data.filter((recipe) => recipe.tags.includes(mycategory));
+  // Generer HTML for hver opskrift
+  markup += data
+    .map(
+      (recipe) =>
+        `
+        <div >
+          <img src="${recipe.image}" alt="" />
+          <h3>${recipe.name}</h3>
+        </div>
+        `
+    )
+    .join("");
 
-//   console.log("Filtered Recipes:", filteredRecipes); // Debugging
+  // Lukker wrapper-div'en
+  markup += `</div>`;
 
-//   // Hvis ingen opskrifter findes, vis besked
-//   if (filteredRecipes.length === 0) {
-//     listContainer.innerHTML = `<p>Ingen opskrifter fundet for kategorien: ${mycategory}</p>`;
-//     return;
-//   }
-
-//   // Begræns til kun at vise 2 opskrifter
-//   const limitedRecipes = filteredRecipes.slice(0, 2);
-
-//   // Generér HTML
-//   const markup = limitedRecipes
-//     .map(
-//       (recipe) => `
-//         <div class="favorites">
-//           <h2>${mycategory}</h2>
-//           <img src="${recipe.image}" alt="${recipe.name}" />
-//           <h3>${recipe.name}</h3>
-//         </div>
-//       `
-//     )
-//     .join("");
-
-//   // Opdater HTML-indholdet
-//   listContainer.innerHTML = markup;
-// }
-
-// // Vis kategoriens navn i `listContainer`
-// document.querySelector(".listcontainer").innerHTML = `<h1>${mycategory}</h1>`;
-
-// console.log("Valgt kategori:", mycategory);
-// console.log("URL search params:", window.location.search);
-// console.log("API Response:", data);
+  listContainer2.innerHTML = markup;
+}
