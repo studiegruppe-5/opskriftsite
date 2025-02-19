cf856d76153e2b3fd4694579d8802a53053d795e
ema <eman0010@stud.kea.dk>
2025-02-19 15:22:34 +0100

    adds dynamic index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 let listContainer = document.querySelector(".category_container");
 let listContainer1 = document.querySelector(".pink_grid");
+let listContainer2 = document.querySelector(".thirdBlock");
+let listContainer3 = document.querySelector(".category_container1");
+let listContainer4 = document.querySelector(".lastBlock");
 
 const mycategory = new URLSearchParams(window.location.search).get("tag");
 
@@ -17,18 +20,35 @@ function showRecipes() {
   fetch(`https://dummyjson.com/recipes`)
     .then((response) => response.json())
     .then((data) => {
-      let recipes = data.recipes.slice(0, limit); // Begræns antallet af opskrifter
-      showPopular(recipes);
-      showOneCategory(recipes.slice(0, 1));
+      let shuffledRecipes = data.recipes.sort(() => Math.random() - 0.5); // Bland rækkefølgen tilfældigt
+
+      // Vælg de første 3 opskrifter til "Popular"
+      let popularRecipes = shuffledRecipes.slice(0, 3);
+
+      // Vælg de næste 3 opskrifter til "Dishes"
+      let dishesRecipes = shuffledRecipes.slice(3, 6);
+
+      // Vis opskrifter i "Popular"
+      showPopular(popularRecipes);
+
+      // Vis én opskrift i "OneRecipe" sektionen
+      showOneRecipe(popularRecipes.slice(0, 1));
+
+      // Vis én opskrift i "OneRecipe2" sektionen
+      showOneRecipe2(popularRecipes.slice(0, 1));
+
+      // Vis de næste 3 opskrifter i "Dishes"
+      showDishes(dishesRecipes);
+
+      showOneRecipe3(popularRecipes.slice(0, 1));
     });
 }
-
-function showPopular(data) {
-  const markup = data
+// done nedaf
+function showPopular(recipes) {
+  const markup = recipes
     .map(
       (recipe) =>
         `
-    
          <a class="category_box" href="https://dummyjson.com/recipes/tag/${recipe.tags[0]}">
          <img src="${recipe.image}" alt="${recipe.name}" />
          <h4>${recipe.tags[0]}</h4>
@@ -39,49 +59,95 @@ function showPopular(data) {
 
   listContainer.innerHTML = markup;
 }
+// done nedaf
+function showOneRecipe() {
+  fetch("https://dummyjson.com/recipes/29")
+    .then((res) => res.json())
+    .then((data) => {
+      listContainer1.innerHTML = `
 
-// function showOneCategory(data) {
-//   const limit = 1; // Begræns til én opskrift
-//   const selectedRecipe = data.find((recipe) => recipe.tags.includes("Mexican")); // Vælg opskriften med tagget 'easy'
+      <img src="${data.image}" alt="${data.name}" />
+      <div class="text_flex">   
+            <h2>Are you busy?</h2>
+             <p>Need meal inspiration? Sometimes, the simplest ingredients can create the most delicious dishes. You should try our mouthwatering ${data.name}</p>
+             <a href="https://dummyjson.com/recipes/29">
+               <button>Udforsk</button>
+             </a>
+        </div>
+            `;
+    });
+}
+// done nedaf
+function showOneRecipe2() {
+  fetch("https://dummyjson.com/recipes/21")
+    .then((res) => res.json())
+    .then((data) => {
+      listContainer2.innerHTML = `
 
-//   if (selectedRecipe) {
-//     const markup = `
-//       <img src="${selectedRecipe.image}" alt="${selectedRecipe.name}" />
-//         <div class="text_flex">
-//           <h2>Are you busy?</h2>
-//           <p>Få inspiration til retter med kort tilberedelsestid og et eller andet andet blaaaaaaaaaaaaaaaaa</p>
-//           <a href="#">
-//             <button>Udforsk</button>
-//           </a>
-//         </div>
-//       `;
-//     listContainer1.innerHTML = markup;
-//   } else {
-//     console.log("Ingen opskrift fundet med det valgte tag.");
-//   }
-// }
+   <div class="grid_1_1">
+          <div class="mushroom_container">
+            <img src="assets/img/mushroom.webp" alt="Mushroom" />
+          </div>
+          <div class="img_container">
+         <img src="${data.image}" alt="${data.name}" />
+          </div>
+          <div class="text_flex">
+            <h3>Healthy options</h3>
 
-function showOneCategory(data) {
-  const limit = 1;
-  const markup = data
+            <div class="text_box">
+              <p>Eating healthy doesn’t have to be boring. Get inspired by our delicious and nutritious ${data.name}</p>
+            </div>
+
+          <a href="https://dummyjson.com/recipes/21">
+               <button>Udforsk</button>
+             </a>
+          </div>
+        </div>
+            `;
+    });
+}
+// done nedaf
+function showDishes(recipes) {
+  const markup = recipes
     .map(
       (recipe) =>
         `
+         <a class="carousel_container" href="https://dummyjson.com/recipes/tag/${recipe.tags[0]}">
          <img src="${recipe.image}" alt="${recipe.name}" />
-          <div class="text_flex">
-            <div class="text_flex">
-            <h2>Are you busy?</h2>
-            <p>Få inspiration til retter med kort tilberedelsestid og et eller andet andet blaaaaaaaaaaaaaaaaa</p>
-            <a href="#">
-              <button>Udforsk</button>
-            </a>
-          </div>
-            `
+         <h4>${recipe.tags[0]}</h4>
+         </a>
+          `
     )
     .join("");
 
-  listContainer1.innerHTML = markup;
+  listContainer3.innerHTML = markup;
 }
 
-// Kald funktionen første gang, når siden indlæses
+function showOneRecipe3() {
+  fetch("https://dummyjson.com/recipes/23")
+    .then((res) => res.json())
+    .then((data) => {
+      listContainer4.innerHTML = `
+
+ <div class="grid_1_1">
+          <div class="img_container">
+         <img src="${data.image}" alt="${data.name}" />
+          </div>
+          <div class="text_flex ">
+         <h3>${data.tags[0]}</h3>
+
+            <div class="text_box">
+              <p>Kan du rigtig meget godt lide kage og alt muligt andet og huller i tænderne så spis alt vores lækre sukkermummelum.</p>
+            </div>
+
+            <button>Udforsk</button>
+          </div>
+          <div class="mitt_container">
+            <img src="assets/img/mitt.webp" alt="Baking mitt" />
+          </div>
+        </div>
+            `;
+    });
+}
+
 showRecipes();
